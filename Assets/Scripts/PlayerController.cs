f7dace2c8fe1653eee97e5fc6c0bc23e6ecc6dcd
Jayjay <jayjayish@gmail.com>
2020-02-15 12:01:11 -0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class PlayerController : PhysicsObject
 {
 
+
+
+    protected Vector2 move;
+
+    [SerializeField]
+    protected float jumpTakeOffSpeed;
+
+
+
+
+
     protected override void FixedUpdate(){
 
         base.FixedUpdate();
@@ -16,81 +27,30 @@ public class PlayerController : PhysicsObject
         move = Vector2.zero;
 
 
-        if (isAttacking)
-        {
-            attackMovementDelegate?.Invoke();
-        }
-        else
-        {
-
-            move.x = Input.GetAxis("Horizontal");
-
-            if (Input.GetButtonDown("Jump") && isGrounded) //checks if jump button is pressed while grounded
-            {
-                velocity.y = jumpTakeOffSpeed;
-            }
-
-            else if (Input.GetButtonUp("Jump")) // reduces velocity when user lets go of jump button
-            {
-                if (velocity.y > 0)
-                    velocity.y = velocity.y * 0.5f;
-            }
-
-            // detect dash
-
-            if (Input.GetButtonDown("DashLeft")) //checks if "a" or left arrow button was pressed
-            {
-
-                float timesinceLastLeft = Time.time - lastLeftTime;
-
-                if (timesinceLastLeft <= DOUBLE_PRESS_TIME)
-                {
-                    dashDirection = -1;
-                    dashTime = startDashTime;//timer for dash
-                    Debug.Log("dashTime:" + dashTime);
-                    Debug.Log("double left");//delta tiime
-                }
-                //Double click
-                else
-                {
-                    //Normal Click
-                    lastLeftTime = Time.time;
-                }
 
 
-            }
+    move.x = InputManager.MainHorizontal();
 
-            if (Input.GetButtonDown("DashRight")) //checks if "d" or right arrow button was pressed
-            {
-
-                float timesinceLastRight = Time.time - lastRightTime;
-
-                if (timesinceLastRight <= DOUBLE_PRESS_TIME)
-                {
-                    dashDirection = 1;
-                    dashTime = startDashTime;//timer for dash
-                    Debug.Log("dashTime:" + dashTime);
-
-                    Debug.Log("double right");//debug delta time
-                }
-                //double click
-                else
-                {
-                    //Normal Click
-                    lastRightTime = Time.time;
-                }
-            }
-
-            if (dashTime > 0)
-            {
-                move.x = dashDirection * dashMultiplier;
-                dashTime -= Time.deltaTime; //Decrease time counter
-            }
-        }
-
-
-        targetVelocity = move * maxSpeed;
-
+    if (InputManager.GetButtonDown("Jump") && isGrounded) //checks if jump button is pressed while grounded
+    {
+        velocity.y = jumpTakeOffSpeed;
     }
+
+    else if (Input.GetButtonUp("Jump")) // reduces velocity when user lets go of jump button
+    {
+        if (velocity.y > 0)
+            velocity.y = velocity.y * 0.5f;
+    }
+
+    // detect dash
+
+
+
+
+
+
+    targetVelocity = move * maxSpeed;
+
+   
 
 }
