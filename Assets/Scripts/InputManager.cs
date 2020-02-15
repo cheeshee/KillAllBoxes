28038efd19d5ac47c190e9bbc8635cd2ac9c014a
cheeshee <amyliu1.618@gmail.com>
2020-02-15 12:14:12 -0800

    changed input manager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,59 +4,45 @@ using UnityEngine;
 
 public static class InputManager{
     
-    #region Player1Input
-    public static float P1Horizontal()
+    
+    public static float GetHorizontal(int player)
     {
         float r = 0.0f;
-        r += Input.GetAxis("Key1_Horizontal");
+        if(player == 1)
+            r += Input.GetAxis("Key1_Horizontal");
+        else if(player == 2)
+            r += Input.GetAxis("Key2_Horizontal");
+
         return r;// Mathf.Clamp(r, -1.0f, 1.0f); //If both joystick and keyboard are being used, clamp between values
     }
 
-    public static float P1Vertical()
+    public static float GetVertical(int player)
     {
         float r = 0.0f;
-        r += Input.GetAxis("Key1_Vertical");
+        if(player == 1)
+            r += Input.GetAxis("Key1_Vertical");
+        else if(player == 2)
+            r += Input.GetAxis("Key2_Vertical");
+            
         return r; // Mathf.Clamp(r, -1.0f, 1.0f);
     }
 
-    public static Vector3 P1Input()
+    public static Vector3 MainInput(int player)
     {
-        return new Vector3(P1Horizontal(), P1Horizontal(), 0.0f );
+        return new Vector3(GetVertical(player), GetHorizontal(player), 0.0f );
     }
 
-    public static bool P1isInteracting()
+    public static bool isInteracting(int player)
     {
-        return Input.GetButton("Action1");
+        if(player == 1)
+            return Input.GetButton("Action1");
+        else if(player == 2)
+            return Input.GetButton("Action2");           
+        else    
+            return false;
     }
-    #endregion Player1Input
+    
 
 
-    #region Player2Input
-
-     public static float P2Horizontal()
-    {
-        float r = 0.0f;
-        r += Input.GetAxis("Key2_Horizontal");
-        return r;// Mathf.Clamp(r, -1.0f, 1.0f); //If both joystick and keyboard are being used, clamp between values
-    }
-
-    public static float P2Vertical()
-    {
-        float r = 0.0f;
-        r += Input.GetAxis("Key2_Vertical");
-        return r;//Mathf.Clamp(r, -1.0f, 1.0f);
-    }
-
-    public static Vector3 P2Input()
-    {
-        return new Vector3(P2Horizontal(), P2Horizontal(), 0.0f );
-    }
-
-    public static bool P2isInteracting()
-    {
-        return Input.GetButton("Action2");
-    }
-
-    #endregion Player2Input
 
 }
