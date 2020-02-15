d2dbc924575e018ae2b00cce08731441314e3eae
T1100CanRunCrysis <60325428+ConfirmedToBeACabbage@users.noreply.github.com>
2020-02-15 14:00:24 -0800

    Collision For Boxes
    Collision for boxes on enter and on exit.

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
-    
-
 
     [Header("Box Attributes")]
     [SerializeField] private Dictionary<string, bool> attributes;
     [SerializeField] private string[] fields = {"fragile", "heavy", "bubble", "wrapping"};
-    [SerializeField] public SpriteRenderer[] pattern;
+    [SerializeField] public transform[] pattern;
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,14 +17,51 @@ public class BoxController : MonoBehaviour
             attributes.Add(attribute, false);
         }
 
-        pattern = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        pattern = gameObject.GetComponentsInChildren<transform>();
         
     }
 
-    
-
     void FixedUpdate(){
 
+        Change_Active();
+
     }
+
+    void Change_Active(){
+
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+
+        if(col.tag == Tag.BUBBLE){
+
+            attribute("bubble", true);
+
+        }
+        if(col.tag == Tag.WRAPPING){
+
+            attribute("wrapping", true);
+
+        }
+
+    }
+
+    void OnCOllisionExit2D(Collision2D col){
+        
+        if(col.tag == Tag.BUBBLE){
+
+            attribute("bubble", false);
+
+        }
+        if(col.tag == Tag.WRAPPING){
+
+            attribute("wrapping", false);
+
+        }
+
+
+    }
+
 
 }
