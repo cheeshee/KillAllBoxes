97e8b8f2ffc162ff0b8b8bbe212fe3068cc6e607
mslongo <mslongo@live.ca>
2020-02-15 13:00:11 -0800

    Box ordering stuffs scripts

---
+++ Assets/Scripts/OrderBehaviourTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderBehaviourTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("U"))
+        {
+            AddOrder();
+        }
+    }
+
+    void AddOrder()
+    {
+
+    }
+}
