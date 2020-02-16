e056afc3405d0988be9d314a95861cf8b58f5d0e
T1100CanRunCrysis <60325428+ConfirmedToBeACabbage@users.noreply.github.com>
2020-02-15 21:10:04 -0800

    Floor Mechanics Prototype
    Prototype for floor mechanics working.

--- Assets/Scripts/FloorController.cs
+++ Assets/Scripts/FloorController.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class FloorController : MonoBehaviour
 {
 
-    /*
-    [Header("Stickers")]
-    public Dictionary<string, bool> stickers;
-
-    [Header("Wrappers")]
-    public Dictionary<string, bool> wrappers;
-    */
-
     [Header("Sticker/Wrapper")]
     public string apply; 
+
 
     void Start(){
 
@@ -22,68 +15,5 @@ public class FloorController : MonoBehaviour
         apply = gameObject.name;
 
     }
-
-    /*
-    void Awake(){
-
-        for(int i = 0; i < 4; i++){
-
-            stickers.Add("Sticker" + (i+1).ToString(), false);
-            wrappers.Add("Wrapper" + (i+1).ToString(), false);
-
-        }
-
-        Set_Stick_Wrap();
-
-    }   
-
-    void Set_Stick_Wrap(){
-
-        //Check if this floor is a sticker or a wrapper floor
-        sticker = false;
-        if(gameObject.tag == "Sticker"){
-
-            sticker = true;
-
-        }   
-
-        if(sticker){
-
-            foreach(KeyValuePair<string, bool> pick in stickers){
-
-                if(pick.Key == gameObject.name){
-
-                    stickers[pick.Key] = true;
-                    apply = pick.Key;
-
-                }
-
-            }
-
-        }
-        else{
-
-            foreach(KeyValuePair<string, bool> pick in wrappers){
-
-                 if(pick.Key == gameObject.name){
-
-                    wrappers[pick.Key] = true;
-                    apply = pick.Key
-
-                }
-
-            }
-
-        }
-
-    }
-
-    public SpriteRenderer Change_Sprite(SpriteRenderer spritechange){
-
-        spritechange.sprite = apply;
-        return 
-
-    }
-    */
 
 }
