b7413427878136036a7b032e36be4fc7c1d7880a
mslongo <mslongo@live.ca>
2020-02-16 15:06:35 -0800

    Timer things working with assets

---
+++ Assets/Scripts/TimerUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUpdate : MonoBehaviour
+{
+    public GlobalTimer currentTime;
+    public Text elementText;
+
+    private void Start()
+    {
+        currentTime = GameObject.Find("LevelTimer").GetComponent<GlobalTimer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        elementText.text = currentTime.timer.ToString("0.0");
+    }
+}
