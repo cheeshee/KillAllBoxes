e2fcd75915373309be2b6b331516b725e52bd184
Jayjay <jayjayish@gmail.com>
2020-02-16 04:03:56 -0800

    Update SpawnController.cs

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -9,13 +9,15 @@ public class SpawnController : MonoBehaviour
     protected float initialSpawnRate = 10;
     protected float spawnRate;
     protected float lastSpawnedBoxTime;
-
+    protected int boxSpawnNumber = 0;
+    private GameObject box;
 
     // Start is called before the first frame update
     protected void Start()
     {
         spawnRate = initialSpawnRate;
         lastSpawnedBoxTime = Time.time;
+        boxSpawnNumber = 0;
     }
 
     // Update is called once per frame
@@ -24,7 +26,29 @@ public class SpawnController : MonoBehaviour
 
         if (Time.time > lastSpawnedBoxTime + spawnRate)
         {
-            StartCoroutine(SpawnBoxSet());
+            if (boxSpawnNumber == 0)
+            {
+                GameObject box = ObjectPooler.Instance.SpawnFromPool(Pool.NORMAL_BOX, transform.position, Quaternion.identity);
+                box.GetComponent<BoxController>().OnObjectSpawn();
+                boxSpawnNumber = 1;
+            }
+            else if (boxSpawnNumber == 1)
+            {
+                box = ObjectPooler.Instance.SpawnFromPool(Pool.HEAVY_BOX, transform.position, Quaternion.identity);
+                box.GetComponent<BoxController>().OnObjectSpawn();
+                boxSpawnNumber = 2;
+            }
+            else if (boxSpawnNumber == 2)
+            {
+                box = ObjectPooler.Instance.SpawnFromPool(Pool.FRAGILE_BOX, transform.position, Quaternion.identity);
+                box.GetComponent<BoxController>().OnObjectSpawn();
+                boxSpawnNumber = 0;
+            }
+            else
+            {
+                Debug.Log("IM VERY ANGRY SPAWNER");
+            }
+            //StartCoroutine(SpawnBoxSet());
             // Debug.Log("Spawn!");
             lastSpawnedBoxTime = Time.time;
         }
