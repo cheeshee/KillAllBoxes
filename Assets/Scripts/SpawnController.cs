b1b5d01fb22f7ba45b54dc402c15f2305219ab89
Jayjay <jayjayish@gmail.com>
2020-02-15 22:51:17 -0800

    Added Spawn MAnager

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class SpawnController : MonoBehaviour
 {
+
+    [SerializeField]
+    protected float initialSpawnRate = 10;
+    protected float spawnRate;
+    protected float lastSpawnedBoxTime;
+
+
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
-        
+        spawnRate = initialSpawnRate;
+        lastSpawnedBoxTime = Time.time;
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
     {
-        
+        if (Time.time > lastSpawnedBoxTime + spawnRate)
+        {
+            StartCoroutine(SpawnBoxSet());
+        }
     }
+
+    IEnumerator SpawnBoxSet()
+    {
+        GameObject box = ObjectPooler.Instance.SpawnFromPool(Pool.NORMAL_BOX, transform.position, Quaternion.identity);
+        box.GetComponent<BoxController>().OnObjectSpawn();
+        yield return new WaitForSeconds(10 * (1f / 60f));
+
+        box = ObjectPooler.Instance.SpawnFromPool(Pool.HEAVY_BOX, transform.position, Quaternion.identity);
+        box.GetComponent<BoxController>().OnObjectSpawn();
+        yield return new WaitForSeconds(10 * (1f / 60f));
+
+
+        box = ObjectPooler.Instance.SpawnFromPool(Pool.FRAGILE_BOX, transform.position, Quaternion.identity);
+        box.GetComponent<BoxController>().OnObjectSpawn();
+
+    }
+
+
+
 }
