afcc262bf97b49cded97f671c61969bf63048097
mslongo <mslongo@live.ca>
2020-02-15 23:22:52 -0800

    Fixed UI

---
+++ Assets/Scripts/HeavyBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavyBox : BoxController
+{
+
+    public override void OnObjectSpawn()
+    {
+        base.OnObjectSpawn();
+        isHeavy = true; 
+    }
+
+
+
+}
